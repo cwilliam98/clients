19a2a7989c8ba85df9d1592491e9ab693e71aaac
cwilliam98 <cristianwillian174@gmail.com>
2020-10-03 22:29:07 -0300

    seeding

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,40 @@
-INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES('Cristian Albrecht', TIMESTAMP WITH TIME ZONE '1998-06-16T20:50:07.12345Z','0','042.675.330-59','100.00')
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Julia Sarah Isabelle da Rocha', TIMESTAMP WITH TIME ZONE '1998-08-12T20:50:07.12345Z','0','502.447.216-02','100.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Carlos Eduardo Bruno João dos Santos', TIMESTAMP WITH TIME ZONE '1973-09-21T12:50:00.12345Z','2','792.679.043-53','2000.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Alessandra Vitória Pires', TIMESTAMP WITH TIME ZONE '1977-08-25T20:50:55.12345Z','4','708.739.408-81','1000.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Sebastião Miguel Silva', TIMESTAMP WITH TIME ZONE '1982-12-08T20:50:32.12345Z','1','573.428.424-60','500.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Benjamin Filipe Moraes', TIMESTAMP WITH TIME ZONE '1969-01-07T20:50:15.12345Z','3','522.038.644-19','200.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Louise Andrea Vitória Santos', TIMESTAMP WITH TIME ZONE '1950-04-12T08:00:00.12345Z','1','598.634.882-67','120.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Carlos Eduardo César Cardoso', TIMESTAMP WITH TIME ZONE '1971-01-05T01:50:50.12345Z','0','907.511.086-34','120.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Larissa Elaine Carolina Lima', TIMESTAMP WITH TIME ZONE '1968-02-03T10:50:52.12345Z','3','854.168.472-55','400.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Nicole Beatriz da Conceição', TIMESTAMP WITH TIME ZONE '1980-04-08T15:50:15.12345Z','6','328.940.456-03','600.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Elza Rosângela Carvalho', TIMESTAMP WITH TIME ZONE '1952-01-23T22:50:14.12345Z','2','773.422.303-67','190.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Gustavo Murilo Iago Araújo', TIMESTAMP WITH TIME ZONE '1962-02-18T21:50:00.12345Z','1','769.431.563-65','900.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Cauê Ruan Fernando da Costa', TIMESTAMP WITH TIME ZONE '1949-12-22T00:50:56.12345Z','0','170.714.032-44','1000.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Antonella Manuela da Silva', TIMESTAMP WITH TIME ZONE '1952-10-03T03:50:35.12345Z','3','065.390.924-15','900.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Nair Carolina Rezende', TIMESTAMP WITH TIME ZONE '1941-01-14T23:50:26.12345Z','0','138.002.906-69','300.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Rayssa Andreia Rebeca Campos', TIMESTAMP WITH TIME ZONE '1956-05-27T10:50:22.12345Z','5','528.346.378-81','650.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Juliana Jaqueline Rosa da Luz', TIMESTAMP WITH TIME ZONE '1958-05-10T00:40:00.12345Z','3','370.404.071-16','60.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Mariah Julia Sueli da Rosa', TIMESTAMP WITH TIME ZONE '1967-04-11T13:50:07.12345Z','0','758.342.852-10','700.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Joaquim Renan Felipe Silveira', TIMESTAMP WITH TIME ZONE '1992-05-14T10:00:57.12345Z','1','329.242.241-87','1200.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Manoel Calebe Galvão', TIMESTAMP WITH TIME ZONE '1989-06-09T05:10:57.12345Z','3','442.203.956-36','900.00');
+INSERT INTO tb_client (name, birth_date, children, cpf, income) 
+VALUES('Bernardo Martin Dias', TIMESTAMP WITH TIME ZONE '1943-06-21T23:50:17.12345Z','1','842.461.161-65','980.00');
