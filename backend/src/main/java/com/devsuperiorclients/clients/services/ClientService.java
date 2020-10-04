e9bb6917374a9da292a304b727e85b8d9f12443e
cwilliam98 <cristianwillian174@gmail.com>
2020-10-03 21:56:49 -0300

    Update ok

--- backend/src/main/java/com/devsuperiorclients/clients/services/ClientService.java
+++ backend/src/main/java/com/devsuperiorclients/clients/services/ClientService.java
@@ -2,6 +2,8 @@ package com.devsuperiorclients.clients.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -41,6 +43,19 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}
+	
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		
 		entity.setName(dto.getName());
